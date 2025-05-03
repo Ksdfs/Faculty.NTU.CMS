8a79b5b43d36e385e1f89d5b9909463fede74a33
nkhanh <147629789+Ksdfs@users.noreply.github.com>
2025-05-03 10:40:35 +0700

    Delete Faculty.NTU.CMS directory

--- Faculty.NTU.CMS/src/main/java/com/thiCK/Nhom16/repository/CategoryPostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.thiCK.Nhom16.repository;
-
-import com.thiCK.Nhom16.enitity.CategoryPost;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryPostRepository extends JpaRepository<CategoryPost, Integer> {
-}
--- Faculty.NTU.CMS/src/main/java/com/thiCK/Nhom16/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.thiCK.Nhom16.repository;
-
-import com.thiCK.Nhom16.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
